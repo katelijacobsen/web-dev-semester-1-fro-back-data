d072692187a1bdc90afbf0e814c86ee24e6b3235
katelijacobsen <katjamaehleke98@gmail.com>
2026-02-11 21:21:07 +0100

    add basic navigation for now

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index.html")
 def index():
     return render_template("index.html")
+
 @app.route("/signup.html")
 def signup():
     return render_template("signup.html")
+
+@app.route("/login.html")
+def login():
+    return render_template("login.html")
